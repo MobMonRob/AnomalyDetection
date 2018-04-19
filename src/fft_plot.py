bd90107bf74028474fe526ffb22bbd60824d7363
benediktbosshammer <bosshammer.benedikt@student.dhbw-karlsruhe.de>
2018-04-19 11:10:19 +0200

    FFT plot

--- src/fft_plot.py
+++ src/fft_plot.py
@@ -7,21 +7,45 @@ Created on Thu Apr 19 10:23:36 2018
 
 import matplotlib.pyplot as plt
 import scipy.fftpack as fpack
+import numpy as np
+
+import platform
+system = platform.system()
+
+if system == 'Darwin':
+    dir_spacer = '/'
+elif system == 'Windows':
+    dir_spacer = '\\'    
+
+# Number of samplepoints
+N = 750
+# sample spacing
+T = 1.0 / 800.0
 
 myarray = []
-with open('..\\data\\Messung_1.csv') as f:
+with open('..' + dir_spacer + 'data' + dir_spacer + 'Messung_1.csv') as f:
     lines = f.readlines()   
     for line in lines:
         split =  line.split(',')
         if (len(split) > 9):
             myarray.append([split[2], split[3], split[4], split[6], split[7], split[8]])
          
-    normal = myarray[:750]
+    normal = myarray[750:]
     transformed = fpack.fft(normal)
+    print(transformed)
+    
+    
+    yf = fpack.fft(normal)
+    xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
+
+    fig, ax = plt.subplots()
+    ax.plot(xf, 2.0/N * np.abs(yf[:N//2]))
+    plt.show()
+    
     
     #plotArray = []
     #for item in myarray:
       #  plotArray.append(item[0:3])
         
-    plt.plot(transformed)
-    plt.show()
+    #plt.plot(transformed)
+    #plt.show()
